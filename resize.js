c4267a059e4ed25e1a31d92251caae6ab126ef6f
Rogier Slag <rogier.slag@gmail.com>
2015-02-22 15:29:43 +0100

    Performance improvements

--- resize.js
+++ resize.js
@@ -8,7 +8,7 @@ server = http.createServer(function (req, res) {
   } else if ( req.method === 'POST' && req.url === '/token' ) {
     createToken(req, res);
   } else if ( req.method === 'POST' ) {
-    uploadImage(res, res);
+    Image.upload(req, res);
   } else {
     res.writeHead(405, 'Method not supported');
   }
@@ -38,9 +38,8 @@ Image.get = function(req, res) {
 
   console.log('Requesting file ' + fileName + ' in ' + fileType + ' format in a ' + resolutionX + 'x' + resolutionY + 'px resolution');
 
-  cache = Image.checkCache(fileName, fileType, resolutionX, resolutionY, res);
-  // If true, it redirected the request
-  if ( !cache ) {
+  if ( !Image.checkCache(fileName, fileType, resolutionX, resolutionY, res) ) {
+    // If we get here, the file was not in the cache
     res.writeHead(200, {'Content-Type': supportedFileType(fileType)})
     Image.encode(fileName, fileType, resolutionX, resolutionY, res);
   }
@@ -50,26 +49,28 @@ Image.checkCache = function(fileName, fileType, resolutionX, resolutionY) {
 }
 Image.encode = function(fileName, fileType, resolutionX, resolutionY, res ) {
   savedLocation = '/vagrant/'+fileName + '_' + resolutionX + 'x' + resolutionY + '.' + fileType;
-  gm('/vagrant/example.png')
+  resized = gm('/vagrant/example.image')
     .options({imageMagick: true})
-    .resize(resolutionX, resolutionY)
-    .stream(fileType, function streamOut(err, stdout, stderr) {
+    .resize(resolutionX, resolutionY);
+  resized.stream(fileType, function streamOut(err, stdout, stderr) {
       stdout.pipe(res);
+      //res.end();
     });
 
- gm('/vagrant/example.png')
-    .options({imageMagick: true})
-    .resize(resolutionX, resolutionY)
-    .toBuffer(fileType, function write(err,stream) {
+  resized.toBuffer(fileType, function write(err,stream) {
       if ( !err) {
         file = fs.createWriteStream(savedLocation);
         file.write(stream);
         // Upload to AWS
+        Image.uploadToCache(fileName, fileType, resolutionX, resolutionY, savedLocation);
       }
     }); 
 }
-Image.uploadToCache = function(fileName, fileType, resolutionX, resolutionY, imageData) {
+Image.uploadToCache = function(fileName, fileType, resolutionX, resolutionY, content) {
   return null;
+}
+Image.upload = function(req, res) {
+  // Upload the RAW image to AWS S3, stripped of its extension
 }
 
 function supportedFileType(fileType) {
@@ -84,6 +85,11 @@ function supportedFileType(fileType) {
     default:
       return null;
   }
+}
+
+function createToken(req, res) {
+  // Here we create a token which is valid for one single upload
+  // This way we can directly send the file here and just a small json payload to the app
 }
 
 server.listen(1337, '10.0.2.15', function() {

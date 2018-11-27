6e4693c5bff3ca888f713e1be1d9714495a2af90
Rogier Slag <rogier.slag@gmail.com>
2018-11-27 13:58:29 +0100

    Batch influx writes

--- src/metrics/influx.js
+++ src/metrics/influx.js
@@ -2,12 +2,38 @@ import {InfluxDB} from 'influx';
 import config from 'config';
 import log from '../log';
 
+const THRESHOLD_IN_MS = 60000;
+
 function setup() {
   const influxConnection = config.get("metrics.influx.dsn");
   const influx = new InfluxDB(influxConnection);
   log('debug', `Connecting influx to ${influxConnection}`);
 
+  let queue = [];
+  let lastWrite = new Date();
   let lastPrintedError;
+  let timeout;
+
+  function drainQueue() {
+    const queueToSend = queue;
+    queue = [];
+    lastWrite = new Date();
+    if (queue.length > 0) {
+      log('debug', 'Draining queue for influx');
+      influx.writePoints(queueToSend).catch(err => {
+        // limit an error to once every 30 seconds
+        if (!lastPrintedError || (new Date() - lastPrintedError) > 30) {
+          log('error', `Error saving data to InfluxDB! ${err.stack}`);
+        }
+        lastPrintedError = new Date();
+      });
+    }
+  }
+
+  process.on('SIGINT', drainQueue);
+  process.on('SIGTERM', drainQueue);
+  process.on('SIGQUIT', drainQueue);
+  process.on('SIGABRT', drainQueue);
 
   return {
     write(metric) {
@@ -17,15 +43,22 @@ function setup() {
         tags: Object.assign({}, metricPoint.tags, {request_type: metricPoint.type}),
         fields: Object.assign({}, metricPoint.fields, {duration: metricPoint.duration})
       };
-      influx.writePoints([
-        influxMetric
-      ]).catch(err => {
-        // limit an error to once every 30 seconds
-        if (!lastPrintedError || (new Date() - lastPrintedError) > 30) {
-          log('error', `Error saving data to InfluxDB! ${err.stack}`);
-        }
-        lastPrintedError = new Date();
-      });
+      queue.push(influxMetric);
+
+      // Now determine whether to actually fire a write (every 250 items or THRESHOLD_IN_MS)
+      if (queue.length > 250 || new Date() - lastWrite > THRESHOLD_IN_MS) {
+        drainQueue();
+      } else {
+        // For the case somebody receives little traffic
+        const innerLast = lastWrite;
+        timeout = setTimeout(() => {
+          if (innerLast !== lastWrite) {
+            log('debug', 'No need to write, another write was performed in the meantime');
+            return;
+          }
+          drainQueue();
+        }, THRESHOLD_IN_MS);
+      }
     }
   };
 }

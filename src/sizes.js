5915d345d883058525965ce707f71a53f867b7d4
Rogier Slag <rogier.slag@gmail.com>
2021-07-13 15:53:52 +0200

    update readme.md

--- src/sizes.js
+++ src/sizes.js
@@ -1,4 +1,4 @@
 import config from 'config';
 
 export const MAX_IMAGE_IN_MP = (config.has('constraints.max_input') && config.get('constraints.max_input')) || 30;
-export const MAX_IMAGE_ON_DISK = (config.has('constraints.max_on_disk') && config.get('constraints.max_on_disk')) || 10;
+export const MAX_IMAGE_ON_DISK = (config.has('constraints.max_on_disk') && config.get('constraints.max_on_disk')) || 30;
